99387e99bd9bb818c0331d6cabeb3248c3937cb9
Ori Harel <ori.harel@taranis.ag>
2023-10-15 00:21:29 +0300

    adding the overlay

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,42 +1,25 @@
-(function() {
-    function displaySearchResults(results, links) {
-      var searchResults = document.getElementById('search-results');
-  
-      if (results.length) { // Are there any results?
-        var appendString = '';
-  
-        for (var i = 0; i < results.length; i++) {  // Iterate over the results
-          var item = links[results[i].ref];
-          appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
-          appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
-        }
-  
-        searchResults.innerHTML = appendString;
-      } else {
-        searchResults.innerHTML = '<li>No results found</li>';
-      }
-    }
-  
-    function getQueryVariable(variable) {
-      var query = window.location.search.substring(1);
-      var vars = query.split('&');
-  
-      for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
-  
-        if (pair[0] === variable) {
-          return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
-        }
-      }
-    }
-  
-    var searchTerm = getQueryVariable('query');
-  
-    if (searchTerm) {
-      document.getElementById('search-box').setAttribute("value", searchTerm);
+function displaySearchResults(results, links) {
 
-        // Initalize lunr with the fields it will be searching on. I've given title
-        // a boost of 10 to indicate matches on this field are more important.
+    var searchResults = document.getElementById('search-results');
+
+    if (results.length) { // Are there any results?
+      var appendString = '';
+
+      for (var i = 0; i < results.length; i++) {  // Iterate over the results
+        var item = links[results[i].ref];
+        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+        appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
+      }
+
+      searchResults.innerHTML = appendString;
+    } else {
+      searchResults.innerHTML = '<li>No results found</li>';
+    }
+}
+
+function search(searchTerm) {
+    if (searchTerm) {
+    
         var idx = lunr(function () {
 
             this.use(lunr.multiLanguage('en', 'he'));
@@ -79,7 +62,25 @@
             })
         });
 
-        var results = idx.search(searchTerm); // Get lunr to perform a search
-        displaySearchResults(results, window.israelLinks); // We'll write this in the next section
+        var results = idx.search(searchTerm);
+        displaySearchResults(results, window.israelLinks);
     }
-  })();
+
+}
+
+ 
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const mobileSearchButton = document.getElementById("mobileSearchButton");
+    const overlayElement = document.getElementById('searchOverlay');
+
+    mobileSearchButton.addEventListener("click", function() {
+        overlayElement.classList.add("active");
+    });
+    
+    overlayElement.addEventListener("click", function() {
+        overlayElement.classList.remove("active");
+    });
+    
+})
