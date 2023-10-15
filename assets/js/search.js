17ab99ca726ed97f6961c34e740291333bd11e45
Yanai Edri <yanai101@gmail.com>
2023-10-15 23:18:55 +0300

    add some css fix

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,220 +1,220 @@
-
-
 function clearResultsList() {
-    var searchResults = document.getElementById('search-results');
-    var searchResultsDiv = document.getElementById('searchResultsDiv');
-    searchResults.innerHTML = '';
-    searchResultsDiv.classList.remove("active");
+  var searchResults = document.getElementById("search-results");
+  var searchResultsDiv = document.getElementById("searchResultsDiv");
+  searchResults.innerHTML = "";
+  searchResultsDiv.classList.remove("active");
 }
 
 function clearInput() {
-    const searchInputWeb = document.getElementById('searchInputWeb');
-    const searchInput = document.getElementById('searchInput');
+  const searchInputWeb = document.getElementById("searchInputWeb");
+  const searchInput = document.getElementById("searchInput");
 
-    searchInputWeb.value = '';
-    searchInput.value = '';
+  searchInputWeb.value = "";
+  searchInput.value = "";
 }
 
-
 function searchResultClicked(aElement) {
-    const overlayElement = document.getElementById('searchOverlay');
-    const overlayElementWeb = document.getElementById('searchOverlayWeb');
+  const overlayElement = document.getElementById("searchOverlay");
+  const overlayElementWeb = document.getElementById("searchOverlayWeb");
 
-    const linkId = aElement.id;
-    const targetElement = document.getElementById(linkId);
+  const linkId = aElement.id;
+  const targetElement = document.getElementById(linkId);
 
-    overlayElement.classList.remove("active");
-    overlayElementWeb.classList.remove("active");
-    clearInput()
-    clearResultsList()
+  overlayElement.classList.remove("active");
+  overlayElementWeb.classList.remove("active");
+  clearInput();
+  clearResultsList();
 
-    window.location.hash = `#${linkId}`;
-    const detailsElement = targetElement.parentElement.parentElement.parentElement.parentElement
-    
-    // const xpathExpression = `//p[@id='${linkId}']/ancestor::details[0]`;
-    // const result = document.evaluate(xpathExpression, document, null, XPathResult.ANY_TYPE, null);
-    // const detailsElement = result.iterateNext();
-    // detailsElement.setAttribute('open', true);
-    setTimeout(()=> {
-        window.scrollBy({
-            top: -250,
-            behavior: "smooth",
-          });
-        detailsElement.setAttribute('open', true);
-    }, 0);
+  window.location.hash = `#${linkId}`;
+  const detailsElement = targetElement.parentElement.parentElement.parentElement.parentElement;
+
+  // const xpathExpression = `//p[@id='${linkId}']/ancestor::details[0]`;
+  // const result = document.evaluate(xpathExpression, document, null, XPathResult.ANY_TYPE, null);
+  // const detailsElement = result.iterateNext();
+  // detailsElement.setAttribute('open', true);
+  setTimeout(() => {
+    window.scrollBy({
+      top: -250,
+      behavior: "smooth",
+    });
+    detailsElement.setAttribute("open", true);
+  }, 0);
 }
 
 function displaySearchResults(results, links) {
+  var searchResults = document.getElementById("search-results");
+  var searchResultsWeb = document.getElementById("search-results-web");
+  var searchResultsDiv = document.getElementById("searchResultsDiv");
 
-    var searchResults = document.getElementById('search-results');
-    var searchResultsWeb = document.getElementById('search-results-web');
-    var searchResultsDiv = document.getElementById('searchResultsDiv');
+  if (results.length) {
+    // Are there any results?
 
-    if (results.length) { // Are there any results?
+    searchResultsDiv.classList.add("active");
+    var appendString = "";
 
-        searchResultsDiv.classList.add("active");
-        var appendString = '';
+    for (let i = 0; i < results.length; i++) {
+      // Iterate over the results
+      let item = links[results[i].ref];
 
-        for (var i = 0; i < results.length; i++) {  // Iterate over the results
-            var item = links[results[i].ref];
+      let title = item.displayName || item.shortDescription;
 
-            let title = item.displayName || item.shortDescription
+      appendString += '<li><div class="searchResultItem"><span class="searchResultTitle">' + title + "</span>";
 
-            appendString += '<li><div class="searchResultItem"><span class="searchResultTitle">' + title + '</span>';
+      const siteUrl = new URL(window.location.href);
+      const permalink = `${siteUrl.origin}#${item.name}`;
 
-            const siteUrl = new URL(window.location.href);
-            const permalink = `${siteUrl.origin}#${item.name}`
-
-            appendString += `<div class="searchResultLinks"><a href="${item.url}" onclick="return clearInput();">קח אותי ליוזמה</a> / <a onclick="searchResultClicked(${item.name})">עוד מידע</a></div></div></li>`;
-            
-        }
-
-        searchResults.innerHTML = appendString;
-        searchResultsWeb.innerHTML = appendString;
-    } else {
-        searchResults.innerHTML = '<li>No results found</li>';
-        searchResultsWeb.innerHTML = '<li>No results found</li>';
-        searchResultsDiv.classList.remove("active");
+      appendString += `<div class="searchResultLinks"><a href="${item.url}" onclick="return clearInput();">קח אותי ליוזמה</a> / <a onclick="searchResultClicked(${item.name})">עוד מידע</a></div></div></li>`;
     }
+
+    searchResults.innerHTML = appendString;
+    searchResultsWeb.innerHTML = appendString;
+  } else {
+    searchResults.innerHTML = "<li>No results found</li>";
+    searchResultsWeb.innerHTML = "<li>No results found</li>";
+    searchResultsDiv.classList.remove("active");
+  }
 }
 
 function prepareIndex() {
+  let linksOnly = {};
+  var idx = lunr(function () {
+    this.use(lunr.multiLanguage("en", "he"));
 
-    let linksOnly = {}
-    var idx = lunr(function () {
+    this.field("displayName");
+    this.field("description");
+    this.field("shortDescription");
 
-        this.use(lunr.multiLanguage('en', 'he'));
+    window.israelLinks.forEach((category) => {
+      // Add the data to lunr
 
-        this.field('displayName');
-        this.field('description');
-        this.field('shortDescription');
+      const CategoryEntry = {
+        id: category.name,
+        name: category.name,
+        displayName: category.displayName,
+        description: category.description,
+        subCategories: category.subCategories,
+      };
 
-        window.israelLinks.forEach((category)=>{ // Add the data to lunr
+      category.subCategories.forEach((subCategory) => {
+        const subCategoryEntry = {
+          id: subCategory.name,
+          displayName: subCategory.displayName,
+        };
 
-            const CategoryEntry = {
-                id: category.name,
-                name: category.name,
-                displayName: category.displayName,
-                description: category.description,
-                subCategories: category.subCategories,
-            }
+        subCategoryEntry.links = subCategory.links.forEach((link) => {
+          const linkEntry = {
+            id: link.name,
+            displayName: link.displayName,
+            shortDescription: link.shortDescription,
+            description: link.description,
+          };
 
-            category.subCategories.forEach((subCategory)=>{
-                const subCategoryEntry =  {
-                    id: subCategory.name,
-                    displayName: subCategory.displayName,
-                }
-
-                subCategoryEntry.links = subCategory.links.forEach((link)=> {
-                    const linkEntry = {
-                        id: link.name,
-                        displayName: link.displayName,
-                        shortDescription: link.shortDescription,
-                        description: link.description
-                    }
-
-                    linksOnly[link.name] = link
-                })
-            })
-        })
-
-        Object.values(linksOnly).forEach((link)=> {
-            const linkEntry = {
-                id: link.name,
-                displayName: link.displayName,
-                shortDescription: link.shortDescription,
-                description: link.description
-            }
-    
-            this.add(linkEntry);
-        })
+          linksOnly[link.name] = link;
+        });
+      });
     });
 
-    window.linksOnly = linksOnly;
-    window.idx = idx;
+    Object.values(linksOnly).forEach((link) => {
+      const linkEntry = {
+        id: link.name,
+        displayName: link.displayName,
+        shortDescription: link.shortDescription,
+        description: link.description,
+      };
 
+      this.add(linkEntry);
+    });
+  });
+
+  window.linksOnly = linksOnly;
+  window.idx = idx;
 }
 
 function search(searchTerm) {
-    if (searchTerm) {
-        var results = window.idx.search(`*${searchTerm}*`);
-        displaySearchResults(results, window.linksOnly);
+  if (searchTerm) {
+    var results = window.idx.search(`*${searchTerm}*`);
+    displaySearchResults(results, window.linksOnly);
 
-        if (window.location.hostname.indexOf('localhost') < 0) {
-            mixpanel.track(`Search`, {
-                searchTerm
-            });
-        }
+    if (window.location.hostname.indexOf("localhost") < 0) {
+      mixpanel.track(`Search`, {
+        searchTerm,
+      });
     }
-    else {
-        clearResultsList()
-    }
+  } else {
+    clearResultsList();
+  }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  prepareIndex();
+  const mobileSearchButton = document.getElementById("mobileSearchButton");
+  const overlayElement = document.getElementById("searchOverlay");
+  const overlayElementWeb = document.getElementById("searchOverlayWeb");
+  const searchInput = document.getElementById("searchInput");
+  const searchInputWeb = document.getElementById("searchInputWeb");
+  const searchDiv = document.getElementById("searchDiv");
+  const pageHeader = document.getElementsByClassName("pageHeader");
+  var searchResultsDiv = document.getElementById("searchResultsDiv");
 
-    prepareIndex();
-    const mobileSearchButton = document.getElementById("mobileSearchButton");
-    const overlayElement = document.getElementById('searchOverlay');
-    const overlayElementWeb = document.getElementById('searchOverlayWeb');
-    const searchInput = document.getElementById('searchInput');
-    const searchInputWeb = document.getElementById('searchInputWeb');
-    const searchDiv = document.getElementById('searchDiv');
-    var searchResultsDiv = document.getElementById('searchResultsDiv');
+  mobileSearchButton.addEventListener("click", function () {
+    overlayElement.classList.add("active");
+  });
 
-    mobileSearchButton.addEventListener("click", function() {
-        overlayElement.classList.add("active");
-    });
+  searchInputWeb.addEventListener("input", function (event) {
+    const searchTerm = event.target.value;
 
-    searchInputWeb.addEventListener("input", function(event) {
-        
-        const searchTerm = event.target.value;
+    if (searchTerm) {
+      overlayElementWeb.classList.add("active");
+    } else {
+      overlayElementWeb.classList.remove("active");
+    }
+    search(searchTerm);
+  });
 
-        if (searchTerm) {
-            overlayElementWeb.classList.add("active");
-        }
-        else {
-            overlayElementWeb.classList.remove("active");
-        }
-        search(searchTerm)
-    });
-    
-    searchInput.addEventListener("input", function(event) {
-        const searchTerm = event.target.value;
-        search(searchTerm)
-    });
+  searchInput.addEventListener("input", function (event) {
+    const searchTerm = event.target.value;
+    search(searchTerm);
+  });
 
-    searchInput.addEventListener("click", function(event) {
+  searchInput.addEventListener("click", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+  });
 
-        event.preventDefault();
-        event.stopPropagation();
-    });
-    
-    searchDiv.addEventListener("click", function(event) {
+  searchDiv.addEventListener("click", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+  });
 
-        event.preventDefault();
-        event.stopPropagation();
-    });
+  overlayElementWeb.addEventListener("click", function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.target.id === "searchOverlayWeb") {
+      clearPopup();
+    }
+  });
+  // searchResultsDiv.addEventListener("click", function(event) {
 
-    // searchResultsDiv.addEventListener("click", function(event) {
+  //     event.preventDefault();
+  //     event.stopPropagation();
+  // });
 
-    //     event.preventDefault();
-    //     event.stopPropagation();
-    // });
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      clearPopup();
+    }
+  });
 
-    document.addEventListener("keydown", function(event) {
-        if (event.key === "Escape") {
-            overlayElement.classList.remove("active");
-            overlayElementWeb.classList.remove("active");
-            const searchInputWeb = document.getElementById('searchInputWeb');
-            const searchInput = document.getElementById('searchInput');
-            searchInputWeb.value = '';
-            searchInput.value = '';
-        }
-    });
-    
-    overlayElement.addEventListener("click", function() {
-        overlayElement.classList.remove("active");
-    });
-    
-})
+  overlayElement.addEventListener("click", function () {
+    overlayElement.classList.remove("active");
+  });
+
+  const clearPopup = () => {
+    overlayElement.classList.remove("active");
+    overlayElementWeb.classList.remove("active");
+    const searchInputWeb = document.getElementById("searchInputWeb");
+    const searchInput = document.getElementById("searchInput");
+    searchInputWeb.value = "";
+    searchInput.value = "";
+  };
+});
