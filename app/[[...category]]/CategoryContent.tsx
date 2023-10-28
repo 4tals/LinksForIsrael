d15136abeec9d0ca3a1013170c467a956322edcf
yanai <yanai101@gmail.com>
2023-10-29 01:01:07 +0200

    Details in popup (#321)
    * long descioption show in popup
    
    * long descioption show in popup
    
    * Squashed commit of the following:
    
    commit 5abdd5a31519a40749c4631fff5c799f095122f1
    Author: Alwyn <idan.4tal@gmail.com>
    Date:   Fri Oct 27 23:30:43 2023 +0300
    
        Add Short Description
    
    commit 16b4cf87f6bc122b46d54a32a5371fb1f973627b
    Author: Daniel Shterenberg <15daniel10@gmail.com>
    Date:   Fri Oct 27 21:44:52 2023 +0300
    
        Use python 3.11 (#323)
    
    commit b39f41c5cb1f8196bf0a04c0cda3a8d2d485f175
    Author: Daniel Shterenberg <15daniel10@gmail.com>
    Date:   Fri Oct 27 17:15:51 2023 +0300
    
        Try fixing some issues with the script (#322)
    
        * Change the ChatGPT query
    
        * use logging.info again
    
    * Update horizontal scrolling on mobile.
    
    * Update CSS
    
    * Merge from Master
    
    * Improve Design
    
    * All the link icon will be clickable
    
    * Fix Scrolling
    
    * Update Display
    
    * Update URL and dialog fix.
    
    ---------
    
    Co-authored-by: Alwyn <idan.4tal@gmail.com>

--- app/[[...category]]/CategoryContent.tsx
+++ app/[[...category]]/CategoryContent.tsx
@@ -1,5 +1,8 @@
-import { Fragment } from "react";
+"use client";
 
+import { Fragment, ReactNode, useState } from "react";
+
+import { DialogModal } from "../components/DialogModal/DialogModal";
 import { Link, SubCategoryData } from "../utils/categories";
 import { ShareButtons } from "./ShareButtons";
 
@@ -12,8 +15,20 @@ export function CategoryContent({
 	subCategories: Array<SubCategoryData>;
 	categoryDescription: string;
 }) {
+	const [openDialog, setOpenDialog] = useState(false);
+	const [description, setDescription] = useState<{
+		body: ReactNode;
+		link: Link;
+	} | null>(null);
+
 	return (
 		<>
+			<DialogModal
+				title={description?.link.displayName || ""}
+				body={description?.body}
+				open={openDialog}
+				toggleModal={setOpenDialog}
+			/>
 			<div className="share-container">
 				<div className="category-description">{categoryDescription}</div>
 				<ShareButtons category={categoryName} />
@@ -25,7 +40,12 @@ export function CategoryContent({
 						{
 							<h3 key={subcategory.name} className="links-subcategory-header">
 								{subcategory.displayName}
-								<LinksList links={subcategory.links} name={subcategory.name} />
+								<LinksList
+									links={subcategory.links}
+									name={subcategory.name}
+									setDescription={setDescription}
+									setOpenDialog={setOpenDialog}
+								/>
 							</h3>
 						}
 					</Fragment>
@@ -35,7 +55,17 @@ export function CategoryContent({
 	);
 }
 
-function LinksList({ name, links }: { name: string; links: Array<Link> }) {
+function LinksList({
+	name,
+	links,
+	setDescription,
+	setOpenDialog,
+}: {
+	name: string;
+	links: Array<Link>;
+	setDescription: (description: { body: ReactNode; link: Link } | null) => void;
+	setOpenDialog: (open: boolean) => void;
+}) {
 	const numLinks = links.length;
 	if (numLinks === 0) {
 		return <p key={name}>בקרוב</p>;
@@ -44,152 +74,193 @@ function LinksList({ name, links }: { name: string; links: Array<Link> }) {
 	return (
 		<div className="links-section-subcategory">
 			{links.map((link) => (
-				<LinkItem link={link} key={link.name} />
+				<LinkItem
+					link={link}
+					key={link.name}
+					setDescription={setDescription}
+					setOpenDialog={setOpenDialog}
+				/>
 			))}
 		</div>
 	);
 }
 
-function LinkItem({ link }: { link: Link }) {
+function LinkItem({
+	link,
+	setDescription,
+	setOpenDialog,
+}: {
+	link: Link;
+	setDescription: (description: { body: ReactNode; link: Link } | null) => void;
+	setOpenDialog: (open: boolean) => void;
+}) {
+	const handleLinkClick = (e: React.MouseEvent) => {
+		if (link.description?.length > 0) {
+			e.preventDefault(); // Prevent redirect
+			showMore(link, setOpenDialog);
+		}
+	};
+
+	const showMore = (link: Link, setOpenDialog: any) => {
+		setDescription({
+			link,
+			body: (
+				<>
+					<p>{link.description}</p>
+					<br />
+					<LinkIcons link={link} />
+					<a href={link.url} target="_blank" className="popup-link-btn">
+						פתח יוזמה
+					</a>{" "}
+				</>
+			),
+		});
+		setOpenDialog(true); // <-- Open the modal
+	};
+
 	return (
 		<li className="links-section-item" key={link.name}>
 			<a
 				href={link.url}
 				target={link.url?.startsWith(".") ? "" : "_blank"}
-				rel="noopener noreferrer"
-				id={link.name}
-				className="links-section-item-title"
+				onClick={handleLinkClick}
 			>
-				{link.displayName}
-				{link.shortDescription !== "" && (
-					<>
-						<br />
-						<span className="links-section-item-short-description">
-							{link.shortDescription}
-						</span>
-					</>
-				)}
-			</a>
-			<div className="link-icons">
 				{link.initiativeImage && (
-					<a href={link.url} target="_blank">
+					<div>
 						<img
 							className="link-initiative-icon"
 							src={link.initiativeImage}
 							alt="Initiative Image"
 						/>
-					</a>
+					</div>
 				)}
-				{link.whatsapp && (
-					<a href={link.whatsapp} target="_blank">
-						<img
-							className="link-icon"
-							src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/598px-WhatsApp_icon.png"
-							alt="WhatsApp Link"
-						/>
-					</a>
-				)}
-				{link.facebook && (
-					<a href={link.facebook} target="_blank">
-						<img
-							className="link-icon"
-							src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Facebook_icon.svg/2048px-Facebook_icon.svg.png"
-							alt="Facebook Link"
-						/>
-					</a>
-				)}
-				{link.telegram && (
-					<a href={link.telegram} target="_blank">
-						<img
-							className="link-icon"
-							src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png"
-							alt="Telegram Link"
-						/>
-					</a>
-				)}
-				{link.drive && (
-					<a href={link.drive} target="_blank">
-						<img
-							className="link-icon"
-							src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/2295px-Google_Drive_icon_%282020%29.svg.png"
-							alt="Google Drive Link"
-						/>
-					</a>
-				)}
-				{link.forms && (
-					<a href={link.forms} target="_blank">
-						<img
-							className="link-icon"
-							src="https://cdn-icons-png.flaticon.com/512/5968/5968528.png"
-							alt="Google Forms Link"
-						/>
-					</a>
-				)}
-				{link.docs && (
-					<a href={link.docs} target="_blank">
-						<img
-							className="link-icon"
-							src="https://cdn4.iconfinder.com/data/icons/free-colorful-icons/360/google_docs.png"
-							alt="Google Docs Link"
-						/>
-					</a>
-				)}
-				{link.website && (
-					<a href={link.website} target="_blank">
-						<img
-							className="link-icon"
-							src="https://cdn-icons-png.flaticon.com/512/6472/6472018.png"
-							alt="Website Link"
-						/>
-					</a>
-				)}
-				{link.discord && (
-					<a href={link.discord} target="_blank">
-						<img
-							className="link-icon"
-							src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiDCYwuxNrkxd_oOUGb0RxYQ5RH_aFzXlxmlgb_183&s"
-							alt="Discord Link"
-						/>
-					</a>
-				)}
-				{link.instagram && (
-					<a href={link.instagram} target="_blank">
-						<img
-							className="link-icon"
-							src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png"
-							alt="Instagram Link"
-						/>
-					</a>
-				)}
-				{link.tiktok && (
-					<a href={link.tiktok} target="_blank">
-						<img
-							className="link-icon"
-							src="https://static.vecteezy.com/system/resources/previews/023/986/921/original/tiktok-logo-tiktok-logo-transparent-tiktok-icon-transparent-free-free-png.png"
-							alt="Tiktok Link"
-						/>
-					</a>
-				)}
-				{link.twitter && (
-					<a href={link.twitter} target="_blank">
-						<img
-							className="link-icon"
-							src="https://cdn-icons-png.flaticon.com/512/124/124021.png"
-							alt="Twitter Link"
-						/>
-					</a>
-				)}
-				{link.portal && (
-					<a href={link.portal} target="_blank">
-						<img
-							className="link-icon"
-							src="https://res.cloudinary.com/dargbitr2/image/upload/v1697228919/LinksForIsrael/r5ysb355egkpyd10jovq.jpg"
-							alt="Portal Link"
-						/>
-					</a>
-				)}
-			</div>
-			{link.description !== "" && <p>{link.description}</p>}
+				<div id={link.name} className="links-section-item-title">
+					{link.displayName}
+					{link.shortDescription !== "" && (
+						<>
+							<br />
+							<span className="links-section-item-short-description">
+								{link.shortDescription}
+							</span>
+						</>
+					)}
+				</div>
+
+				<LinkIcons link={link} />
+			</a>
 		</li>
 	);
 }
+
+const LinkIcons = ({ link }: { link: Link }) => (
+	<div className="link-icons">
+		{link.whatsapp && (
+			<a href={link.whatsapp} target="_blank">
+				<img
+					className="link-icon"
+					src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/598px-WhatsApp_icon.png"
+					alt="WhatsApp Link"
+				/>
+			</a>
+		)}
+		{link.facebook && (
+			<a href={link.facebook} target="_blank">
+				<img
+					className="link-icon"
+					src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Facebook_icon.svg/2048px-Facebook_icon.svg.png"
+					alt="Facebook Link"
+				/>
+			</a>
+		)}
+		{link.telegram && (
+			<a href={link.telegram} target="_blank">
+				<img
+					className="link-icon"
+					src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png"
+					alt="Telegram Link"
+				/>
+			</a>
+		)}
+		{link.drive && (
+			<a href={link.drive} target="_blank">
+				<img
+					className="link-icon"
+					src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/2295px-Google_Drive_icon_%282020%29.svg.png"
+					alt="Google Drive Link"
+				/>
+			</a>
+		)}
+		{link.forms && (
+			<a href={link.forms} target="_blank">
+				<img
+					className="link-icon"
+					src="https://cdn-icons-png.flaticon.com/512/5968/5968528.png"
+					alt="Google Forms Link"
+				/>
+			</a>
+		)}
+		{link.docs && (
+			<a href={link.docs} target="_blank">
+				<img
+					className="link-icon"
+					src="https://cdn4.iconfinder.com/data/icons/free-colorful-icons/360/google_docs.png"
+					alt="Google Docs Link"
+				/>
+			</a>
+		)}
+		{link.website && (
+			<a href={link.website} target="_blank">
+				<img
+					className="link-icon"
+					src="https://cdn-icons-png.flaticon.com/512/6472/6472018.png"
+					alt="Website Link"
+				/>
+			</a>
+		)}
+		{link.discord && (
+			<a href={link.discord} target="_blank">
+				<img
+					className="link-icon"
+					src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiDCYwuxNrkxd_oOUGb0RxYQ5RH_aFzXlxmlgb_183&s"
+					alt="Discord Link"
+				/>
+			</a>
+		)}
+		{link.instagram && (
+			<a href={link.instagram} target="_blank">
+				<img
+					className="link-icon"
+					src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png"
+					alt="Instagram Link"
+				/>
+			</a>
+		)}
+		{link.tiktok && (
+			<a href={link.tiktok} target="_blank">
+				<img
+					className="link-icon"
+					src="https://static.vecteezy.com/system/resources/previews/023/986/921/original/tiktok-logo-tiktok-logo-transparent-tiktok-icon-transparent-free-free-png.png"
+					alt="Tiktok Link"
+				/>
+			</a>
+		)}
+		{link.twitter && (
+			<a href={link.twitter} target="_blank">
+				<img
+					className="link-icon"
+					src="https://cdn-icons-png.flaticon.com/512/124/124021.png"
+					alt="Twitter Link"
+				/>
+			</a>
+		)}
+		{link.portal && (
+			<a href={link.portal} target="_blank">
+				<img
+					className="link-icon"
+					src="https://res.cloudinary.com/dargbitr2/image/upload/v1697228919/LinksForIsrael/r5ysb355egkpyd10jovq.jpg"
+					alt="Portal Link"
+				/>
+			</a>
+		)}
+	</div>
+);
---
+++ app/components/DialogModal/DialogModal.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { ReactNode, useEffect, useRef } from "react";
+
+import styles from "../AddSite/AddSite.module.scss";
+import "./dialogModal.scss";
+
+type DialogModalProps = {
+	title: string;
+	body: ReactNode;
+	open?: boolean;
+	toggleModal: (open: boolean) => void;
+};
+
+export function DialogModal({
+	title,
+	body,
+	open,
+	toggleModal,
+}: DialogModalProps) {
+	const dialogRef = useRef<HTMLDialogElement>(null);
+
+	useEffect(() => {
+		if (body && !open) {
+			toggleModal(true);
+		}
+	}, [body, open, toggleModal]);
+
+	const closeDialog = () => {
+		dialogRef.current?.close();
+	};
+
+	const openDialog = () => {
+		dialogRef.current?.showModal();
+	};
+
+	open && body ? openDialog() : closeDialog();
+
+	return (
+		<dialog id="dialogBox" className="dialogBox" ref={dialogRef}>
+			<header>
+				<h2 className="header">{title}</h2>
+				<button onClick={closeDialog} id="closeDialogHeader">
+					X
+				</button>
+			</header>
+
+			<section className="dialogContent">{body}</section>
+		</dialog>
+	);
+}
