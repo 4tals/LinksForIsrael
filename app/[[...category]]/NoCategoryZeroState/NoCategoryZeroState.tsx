8f96de5c7fb2d537cfac8db4f0b7c2eb2d366c77
Idan Portal <idan.4tal@gmail.com>
2023-10-29 23:37:14 +0200

    Remove from mobile view. (#333)

--- app/[[...category]]/NoCategoryZeroState/NoCategoryZeroState.tsx
+++ app/[[...category]]/NoCategoryZeroState/NoCategoryZeroState.tsx
@@ -4,9 +4,9 @@ import "./noCategoryZeroState.scss";
 
 export const NoCategoryZeroState = () => {
 	return (
-		<div className="container">
+		<div className="welcome-message-container">
 			<span>ברוכים הבאים לפורטל לינק לישראל</span>
-			<span>כאן תוכלו למצוא מגוון לינקים אמינים לכל מה שנוגע למלחמה!</span>
+			<span>מרכזים מגוות יוזמות בתחומים שונים!</span>
 			<span>בתפריט מימין תוכלו לבחור את הקטגוריה הרלוונטית עבורכם</span>
 			<span>ביחד ננצח!</span>
 		</div>
