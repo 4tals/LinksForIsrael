ee1b15676ccdfca7f97a1557ccc8abaa0b3732f7
Alwyn <idan.4tal@gmail.com>
2023-10-30 23:45:14 +0200

    mix-panel & clear horizonal scrolling on mobile

--- app/[[...category]]/CategoriesList.tsx
+++ app/[[...category]]/CategoriesList.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useEffect, useRef } from "react";
 
+import { analyticsService } from "../analytics";
 import { Category } from "../utils/categories";
 import { CategoryContent } from "./CategoryContent";
 
@@ -54,51 +55,47 @@ export function CategoriesList({
 	return (
 		<div className="desktop-category">
 			<div dir="rtl">
-				{categories.map((category) => {
-					return (
-						<div
-							ref={ref}
-							className={[
-								"links-section",
-								category.id === categoryId && "desktop-open",
-							]
-								.filter(Boolean)
-								.join(" ")}
-							id={category.name}
+				{categories.map((category) => (
+					<div
+						ref={ref}
+						className={[
+							"links-section",
+							category.id === categoryId && "desktop-open",
+						]
+							.filter(Boolean)
+							.join(" ")}
+						id={category.name}
+						key={category.name}
+					>
+						<Link
+							href={`/${category.id}`}
 							key={category.name}
+							replace
+							className="w-full"
+							onClick={() => analyticsService.trackCategoryView(category.id)}
 						>
-							<Link
-								href={`/${category.id}`}
-								key={category.name}
-								replace
-								className="w-full"
-							>
-								<div className="links-section-title">
-									{category.image && (
-										<img
-											src={category.image}
-											alt={`${category.displayName} Icon`}
-											className="category-icon"
-										/>
-									)}
-									<h2 className="text-xl">{category.displayName}</h2>
-									{/* <div className="open-caret">
-										<FontAwesomeIcon icon={faAngleDown} />
-									</div> */}
-								</div>
-							</Link>
-							{category.id === categoryId && (
-								<div className="links-section-content sm:hidden">
-									<CategoryContent
-										subCategories={category.subCategories}
-										categoryName={category.displayName}
-										categoryDescription={category.description}
+							<div className="links-section-title">
+								{category.image && (
+									<img
+										src={category.image}
+										alt={`${category.displayName} Icon`}
+										className="category-icon"
 									/>
-								</div>
-							)}
-						</div>
-					);
-				})}
+								)}
+								<h2 className="text-xl">{category.displayName}</h2>
+							</div>
+						</Link>
+						{category.id === categoryId && (
+							<div className="links-section-content sm:hidden">
+								<CategoryContent
+									subCategories={category.subCategories}
+									categoryName={category.displayName}
+									categoryDescription={category.description}
+								/>
+							</div>
+						)}
+					</div>
+				))}
 			</div>
 		</div>
 	);
