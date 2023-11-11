4bb29471850c9362024d461293dc54e2913eeff7
Omer Friedman <omerfriedman@microsoft.com>
2023-11-11 23:56:33 +0200

    fix zero state span lines

--- app/[[...category]]/NoCategoryZeroState/NoCategoryZeroState.tsx
+++ app/[[...category]]/NoCategoryZeroState/NoCategoryZeroState.tsx
@@ -14,7 +14,7 @@ export const NoCategoryZeroState = () => {
 				<span>ברוכים הבאים לפורטל לינק לישראל</span>
 				<span>מרכזים מגוון יוזמות בתחומים שונים</span>
 				<span>בתפריט מימין תוכלו לבחור את הקטגוריה הרלוונטית עבורכם</span>
-				<span>ביחד ננצח!</span>
+				<span className="larger">ביחד ננצח!</span>
 			</div>
 		</div>
 	);
