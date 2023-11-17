7ddec5556d0215b07290f68aaf8a45118b15826d
Alwyn <idan.4tal@gmail.com>
2023-11-18 00:49:29 +0200

    move filterCategories and getResults from useSearch

--- app/components/Search/useSearch.ts
+++ app/components/Search/useSearch.ts
@@ -5,6 +5,63 @@ import { Category, Link, SubCategoryData } from "@/app/utils/categories";
 
 import { mapStringToHebrew } from "./SearchHelpers";
 
+function filterCategories(
+	categories: Category[],
+	searchTerm: string,
+): Category[] {
+	const filteredCategories: Category[] = [];
+	categories.forEach((category) => {
+		const filteredSubCategories = category.subCategories
+			.map((subCategory) => {
+				const filteredLinks: Link[] = subCategory.links.filter((link) => {
+					const lowerSearchTerm = searchTerm.toLowerCase();
+					return (
+						link.name?.toLowerCase().includes(lowerSearchTerm) ||
+						link.displayName?.toLowerCase().includes(lowerSearchTerm) ||
+						link.description?.toLowerCase().includes(lowerSearchTerm) ||
+						link.shortDescription?.toLowerCase().includes(lowerSearchTerm) ||
+						link.tags?.some((tag) =>
+							tag.toLowerCase().includes(lowerSearchTerm),
+						)
+					);
+				});
+
+				return { ...subCategory, links: filteredLinks };
+			})
+			.filter((subCategory) => subCategory.links.length > 0);
+
+		if (filteredSubCategories.length > 0) {
+			filteredCategories.push({
+				...category,
+				subCategories: filteredSubCategories,
+			});
+		}
+	});
+
+	return filteredCategories;
+}
+
+function getResults(
+	categories: Category[],
+	generalAssistanceSubCategory: SubCategoryData,
+	searchTerm: string,
+) {
+	if (!searchTerm) {
+		return [];
+	}
+	const results = filterCategories(categories, searchTerm);
+	if (results.length !== 0) {
+		return results;
+	}
+
+	const mappedSearchTerm = mapStringToHebrew(searchTerm);
+	if (!mappedSearchTerm) {
+		return [];
+	}
+
+	return filterCategories(categories, mappedSearchTerm);
+}
+
 export function useSearch(
 	categories: Category[],
 	generalAssistanceSubCategory: SubCategoryData,
@@ -19,71 +76,32 @@ export function useSearch(
 		setIsMobileSearchOpen((prevState) => !prevState);
 	}, [setIsMobileSearchOpen]);
 
-	function filterCategories(searchTerm: string): Category[] {
-		const filteredCategories: Category[] = [];
-		categories.forEach((category) => {
-			const filteredSubCategories = category.subCategories
-				.map((subCategory) => {
-					const filteredLinks: Link[] = subCategory.links.filter((link) => {
-						const lowerSearchTerm = searchTerm.toLowerCase();
-						return (
-							link.name?.toLowerCase().includes(lowerSearchTerm) ||
-							link.displayName?.toLowerCase().includes(lowerSearchTerm) ||
-							link.description?.toLowerCase().includes(lowerSearchTerm) ||
-							link.shortDescription?.toLowerCase().includes(lowerSearchTerm) ||
-							link.tags?.some((tag) =>
-								tag.toLowerCase().includes(lowerSearchTerm),
-							)
-						);
-					});
-
-					return { ...subCategory, links: filteredLinks };
-				})
-				.filter((subCategory) => subCategory.links.length > 0);
-
-			if (filteredSubCategories.length > 0) {
-				filteredCategories.push({
-					...category,
-					subCategories: filteredSubCategories,
-				});
-			}
-		});
-
-		return filteredCategories;
-	}
-
 	function handleChangeSearch(searchTerm: string) {
 		setSearch(searchTerm);
-		const results = getResults(searchTerm);
+
+		let results = getResults(
+			categories,
+			generalAssistanceSubCategory,
+			searchTerm,
+		);
 		setDisplaySearchResults(results);
 
 		let usedHebrewMapping = false;
+
 		if (results.length === 0) {
 			const mappedSearchTerm = mapStringToHebrew(searchTerm);
 			if (mappedSearchTerm) {
 				usedHebrewMapping = true;
-				setDisplaySearchResults(getResults(mappedSearchTerm));
+				results = getResults(
+					categories,
+					generalAssistanceSubCategory,
+					mappedSearchTerm,
+				);
+				setDisplaySearchResults(results);
 			}
 		}
 
 		analyticsService.trackSearch(searchTerm, results.length, usedHebrewMapping);
-
-		function getResults(searchTerm: string) {
-			if (!searchTerm) {
-				return [];
-			}
-			const results = filterCategories(searchTerm);
-			if (results.length !== 0) {
-				return results;
-			}
-
-			const mappedSearchTerm = mapStringToHebrew(searchTerm);
-			if (!mappedSearchTerm) {
-				return [];
-			}
-
-			return filterCategories(mappedSearchTerm);
-		}
 	}
 
 	return {
