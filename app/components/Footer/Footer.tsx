381a2999481691b7c65fe73fdab90855cddd3757
Alwyn <idan.4tal@gmail.com>
2023-11-20 00:14:26 +0200

    Update footer sizes.

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,69 +1,50 @@
 import { HelpUsButton } from "./HelpUsButton";
 
+const socialLinks = [
+	{
+		href: "https://www.instagram.com/links_for_israel/",
+		imgSrc: "/images/instagram_icon.png",
+		alt: "Instagram",
+	},
+	{
+		href: "https://www.facebook.com/LinksForIsrael/",
+		imgSrc:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/1024px-2021_Facebook_icon.svg.png",
+		alt: "Facebook",
+	},
+	{
+		href: "https://github.com/4tals/LinksForIsrael/",
+		imgSrc: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+		alt: "GitHub",
+	},
+	{
+		href: "https://twitter.com/linksforisrael",
+		imgSrc:
+			"https://upload.wikimedia.org/wikipedia/commons/c/ce/X_logo_2023.svg",
+		alt: "Twitter",
+	},
+	{
+		href: "https://www.linkedin.com/company/links-for-israel/",
+		imgSrc:
+			"https://upload.wikimedia.org/wikipedia/commons/9/90/Wbseries_linkdin.png",
+		alt: "Linkedin",
+	},
+];
+
 export function Footer() {
 	return (
 		<footer className="footer">
 			<div className="social-links">
-				<a
-					href="https://www.instagram.com/links_for_israel/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img
-						src="/images/instagram_icon.png"
-						alt="Instagram"
-						width="32"
-						height="32"
-					/>
-				</a>
-				<a
-					href="https://www.facebook.com/LinksForIsrael/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img
-						src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/1024px-2021_Facebook_icon.svg.png?20220821121039"
-						alt="Facebook"
-						width="32"
-						height="32"
-					/>
-				</a>
-				<a
-					href="https://github.com/4tals/LinksForIsrael/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img
-						src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
-						alt="GitHub"
-						width="32"
-						height="32"
-					/>
-				</a>
-				<a
-					href="https://twitter.com/linksforisrael"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img
-						src="https://upload.wikimedia.org/wikipedia/commons/c/ce/X_logo_2023.svg"
-						alt="Twitter"
-						width="32"
-						height="32"
-					/>
-				</a>
-				<a
-					href="https://www.linkedin.com/company/links-for-israel/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img
-						src="https://upload.wikimedia.org/wikipedia/commons/9/90/Wbseries_linkdin.png"
-						alt="Linkedin"
-						width="32"
-						height="32"
-					/>
-				</a>
+				{socialLinks.map((link) => (
+					<a
+						key={link.href}
+						href={link.href}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img src={link.imgSrc} alt={link.alt} width="15" height="15" />
+					</a>
+				))}
 				<HelpUsButton />
 			</div>
 			<div className="quote-link flex flex-row items-center justify-center">
@@ -77,7 +58,7 @@ export function Footer() {
 						src="/images/reverse.png"
 						alt="Initiative Icon"
 						className="footer-icon"
-						width="40px"
+						width="40"
 					/>
 				</a>
 			</div>
