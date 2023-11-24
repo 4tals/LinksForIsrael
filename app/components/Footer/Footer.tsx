28007f2d73708420ea4e14c8836256ae861e6318
Alwyn <idan.4tal@gmail.com>
2023-11-25 01:19:42 +0200

    Update Footer.

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 import { socialLinks } from "@/app/utils/consts";
-import { Flex, Box, Link, Image, Text } from "@chakra-ui/react";
+import {
+	Flex,
+	Box,
+	Link,
+	Image,
+	Text,
+	IconButton,
+	Stack,
+} from "@chakra-ui/react";
 
 import { HelpUsButton } from "./HelpUsButton";
 
@@ -10,14 +18,14 @@ export function Footer() {
 		<Box
 			as="footer"
 			display="flex"
-			fontFamily="'Roboto', sans-serif"
+			flexDirection={{ base: "column", md: "row" }}
+			fontFamily="Roboto, sans-serif"
 			justifyContent="space-between"
 			alignItems="center"
 			padding="10px"
-			borderTop="1px solid"
+			borderTopWidth="1px"
 			borderColor="gray.200"
 			backgroundColor="white"
-			height={["7svh", "7vh"]} // Responsive height
 			zIndex="999"
 			position="sticky"
 			bottom="0"
@@ -27,11 +35,7 @@ export function Footer() {
 				justify="center"
 				fontSize="14px"
 				color="gray.600"
-				sx={{
-					"@media only screen and (max-width: 768px)": {
-						// Add any mobile-specific styles here
-					},
-				}}
+				mb={{ base: 4, md: 0 }}
 			>
 				<Text textAlign="center" mr="2">
 					הופכים עליהם, טובים אותם
@@ -43,27 +47,31 @@ export function Footer() {
 					<Image
 						src="/images/reverse.png"
 						alt="Initiative Icon"
-						boxSize="40px"
+						boxSize={{ base: "30px", md: "40px" }}
 					/>
 				</Link>
 			</Flex>
-			<HelpUsButton />
-			<Flex
+			{/* HelpUsButton will only display on non-mobile (medium and above) screens */}
+			<Box display={{ base: "none", md: "block" }}>
+				<HelpUsButton />
+			</Box>
+			<Stack
+				direction={{ base: "row", md: "row" }}
 				align="center"
-				gap={["7px", "15px"]} // Responsive gap
-				sx={{}}
+				justify="center"
+				spacing={2}
 			>
 				{socialLinks.map((link) => (
-					<Link key={link.href} href={link.href} isExternal mx="1">
-						<Image
-							src={link.imgSrc}
-							alt={link.alt}
-							boxSize="25px"
+					<Link key={link.href} href={link.href} isExternal>
+						<IconButton
+							aria-label={link.alt}
+							icon={<Image src={link.imgSrc} alt={link.alt} boxSize="25px" />}
+							variant="unstyled"
 							_hover={{ opacity: 0.7 }}
 						/>
 					</Link>
 				))}
-			</Flex>
+			</Stack>
 		</Box>
 	);
 }
