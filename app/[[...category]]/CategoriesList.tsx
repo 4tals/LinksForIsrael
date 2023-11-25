b73b0773c084393dc16b7039d4950ff52967f108
Alwyn <idan.4tal@gmail.com>
2023-11-25 04:25:56 +0200

    Update

--- app/[[...category]]/CategoriesList.tsx
+++ app/[[...category]]/CategoriesList.tsx
@@ -1,78 +1,97 @@
 "use client";
 
 import Link from "next/link";
-import { useContext, useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 
 import { SearchContext } from "@/app/components/RootApp";
 import { withScroll } from "@/app/components/withScroll/withScroll";
+import {
+	Box,
+	Flex,
+	Image,
+	Text,
+	Tooltip,
+	useMediaQuery,
+} from "@chakra-ui/react";
 
 import { analyticsService } from "../utils/analytica/analytics";
-import { Category } from "../utils/categories";
+import { Category as CategoryType } from "../utils/categories";
 
-const getNumberOfInitiatives = (category: Category) =>
+interface CategoriesListProps {
+	categories: CategoryType[];
+	categoryId?: string;
+}
+
+const getNumberOfInitiatives = (category: CategoryType) =>
 	category.subCategories.reduce(
-		(acc, subcategory) => acc + subcategory.links.length,
+		(acc: number, subcategory) => acc + subcategory.links.length,
 		0,
 	);
 
-const CategoriesList = ({
+const CategoriesList: React.FC<CategoriesListProps> = ({
 	categories,
 	categoryId,
-}: {
-	categories: Array<Category>;
-	categoryId?: string;
 }) => {
 	const { search, onSearch } = useContext(SearchContext);
+	const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
+
 	useEffect(() => {
-		const selected = document.querySelector(".links-section.desktop-open");
-		if (!selected) return;
-		selected.scrollIntoView({
-			behavior: "auto",
-			inline: "center",
-			block: "nearest",
-		});
-	}, []);
+		if (isLargerThan768) {
+			const selected = document.querySelector(".links-section.desktop-open");
+			if (selected)
+				selected.scrollIntoView({ behavior: "smooth", block: "nearest" });
+		}
+	}, [isLargerThan768]);
 
 	return (
-		<div dir="rtl" className="icon-navbar">
+		<Flex
+			direction="row"
+			wrap="nowrap" // Prevent wrapping
+			justifyContent="center"
+			p={4}
+			alignItems="center"
+			//   overflowX="auto" // Allow horizontal scrolling
+		>
 			{categories.map((category) => (
-				<div
-					className={[
-						"links-section",
-						!search && category.id === categoryId && "desktop-open",
-					]
-						.filter(Boolean)
-						.join(" ")}
-					id={category.name}
+				<Tooltip
+					label={category.displayName}
+					placement="top"
+					hasArrow
 					key={category.name}
 				>
-					<Link
-						href={`/${category.id}`}
-						key={category.name}
-						replace
-						className="w-full"
+					<Box
+						id={category.name}
 						onClick={() => {
 							analyticsService.trackCategoryView(category.id);
 							onSearch("");
 						}}
+						cursor="pointer"
+						m={2}
+						p={2}
+						borderWidth="1px"
+						borderRadius="md"
+						_hover={{ boxShadow: "lg", transform: "translateY(-2px)" }}
+						bg={!search && category.id === categoryId ? "blue.200" : "white"}
+						transition="all 0.2s ease-in-out"
 					>
-						<div className="links-section-title">
+						<Link href={`/${category.id}`}>
 							{category.image && (
-								<img
+								<Image
 									src={category.image}
 									alt={`${category.displayName} Icon`}
-									className="category-icon"
+									boxSize="40px"
+									objectFit="cover"
+									mx="auto"
 								/>
 							)}
-							<span>{category.displayName}</span>
-							<span className="numbers">
-								{getNumberOfInitiatives(category)}
-							</span>
-						</div>
-					</Link>
-				</div>
+							<Text fontSize="xs" fontWeight="bold" mt={2} textAlign="center">
+								{category.displayName}
+							</Text>
+						</Link>
+					</Box>
+				</Tooltip>
 			))}
-		</div>
+		</Flex>
 	);
 };
 
