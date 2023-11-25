12e3cbd3ae84aa27446012e35c18ccc5211edb2f
Alwyn <idan.4tal@gmail.com>
2023-11-25 02:49:38 +0200

    Update Link

--- app/[[...category]]/LinkItem.tsx
+++ app/[[...category]]/LinkItem.tsx
@@ -1,22 +1,21 @@
-"use client";
+import React from "react";
 
-import { ReactNode } from "react";
+import { Box, Flex, Link as ChakraLink, Image, Text } from "@chakra-ui/react";
 
-import { Link } from "../utils/categories";
+import { Link as LinkType } from "../utils/categories";
 import { LinkIcons } from "./LinkIcons";
 
-const containsHebrewLetters = (str: string) => {
-	// https://jcuenod.github.io/bibletech/2017/08/19/matching-hebrew-unicode-regex/
-	return /[\u0590-\u05EA]/.test(str);
-};
+const containsHebrewLetters = (str: string) => /[\u0590-\u05EA]/.test(str);
 
 export function LinkItem({
 	link,
 	setDescription,
 	setOpenDialog,
 }: {
-	link: Link;
-	setDescription: (description: { body: ReactNode; link: Link } | null) => void;
+	link: LinkType;
+	setDescription: (
+		description: { body: React.ReactNode; link: LinkType } | null,
+	) => void;
 	setOpenDialog: (open: boolean) => void;
 }) {
 	const handleLinkClick = (e: React.MouseEvent) => {
@@ -26,17 +25,17 @@ export function LinkItem({
 		}
 	};
 
-	const showMore = (link: Link, setOpenDialog: any) => {
+	const showMore = (link: LinkType, setOpenDialog: any) => {
 		setDescription({
 			link,
 			body: (
 				<>
-					<p>{link.description}</p>
+					<Text>{link.description}</Text>
 					<br />
 					<LinkIcons link={link} />
-					<a href={link.url} target="_blank" className="popup-link-btn">
+					<ChakraLink href={link.url} isExternal>
 						פתח יוזמה
-					</a>{" "}
+					</ChakraLink>
 				</>
 			),
 		});
@@ -44,40 +43,38 @@ export function LinkItem({
 	};
 
 	return (
-		<div
-			className={`links-section-item 
-				${
-					!containsHebrewLetters(`${link.displayName}${link?.shortDescription}`)
-						? "ltr"
-						: ""
-				}`}
+		<Box
+			className={`links-section-item ${
+				!containsHebrewLetters(`${link.displayName}${link?.shortDescription}`)
+					? "ltr"
+					: ""
+			}`}
 			key={link.name}
+			onClick={handleLinkClick}
 		>
 			{link.initiativeImage && (
-				<div
-					className="link-initiative-icon-bg"
-					style={{ backgroundImage: `url("${link.initiativeImage}")` }}
-				></div>
+				<Image
+					src={link.initiativeImage}
+					alt={link.displayName}
+					position="absolute"
+					top="0"
+					left="5%"
+					width="90%"
+					objectFit="contain"
+					opacity="0.2"
+				/>
 			)}
-			<a
-				href={link.url}
-				target={link.url?.startsWith(".") ? "" : "_blank"}
-				onClick={handleLinkClick}
-			>
-				<div id={link.name} className="links-section-item-title">
+			<Flex direction="column" p="16px 24px">
+				<Text fontSize="17px" fontWeight="900" noOfLines={1}>
 					{link.displayName}
-					{link.shortDescription !== "" && (
-						<>
-							<br />
-							<span className="links-section-item-short-description">
-								{link.shortDescription}
-							</span>
-						</>
-					)}
-				</div>
-
+				</Text>
+				{link.shortDescription && (
+					<Text fontSize="14px" fontWeight="600" noOfLines={2}>
+						{link.shortDescription}
+					</Text>
+				)}
 				<LinkIcons link={link} limit={3} />
-			</a>
-		</div>
+			</Flex>
+		</Box>
 	);
 }
