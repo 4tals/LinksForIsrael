eaa922faebaec4b1a993198c3bad7ebeac5de1a7
Alwyn <idan.4tal@gmail.com>
2023-11-29 23:55:33 +0200

    Remove More CSS and Move another component to Chakra

--- app/components/Search/MobileSearchInput.tsx
+++ app/components/Search/MobileSearchInput.tsx
@@ -1,19 +1,28 @@
-"use client";
-
-import { useContext } from "react";
+import React, { useContext } from "react";
 
 import { SearchContext } from "@/app/components/RootApp";
 import { SearchInput } from "@/app/components/Search/SearchInput";
+import { Box, useMediaQuery } from "@chakra-ui/react";
 
 export function MobileSearchInput() {
 	const { isMobileSearchOpen } = useContext(SearchContext);
-	return (
-		<div
-			className={`mobile-search mobile-search-${
-				isMobileSearchOpen ? "open" : "close"
-			}`}
+	const [isMobile] = useMediaQuery("(max-width: 768px)");
+
+	return isMobile ? (
+		<Box
+			position="absolute"
+			width="100%"
+			zIndex="999"
+			display="flex"
+			justifyContent="center"
+			alignItems="center"
+			padding="0 20px"
+			backgroundColor="rgba(0, 0, 0, 0.2)"
+			overflow="hidden"
+			height={isMobileSearchOpen ? "7vh" : "0"}
+			transition="height 0.1s ease-in-out"
 		>
 			<SearchInput />
-		</div>
-	);
+		</Box>
+	) : null;
 }
