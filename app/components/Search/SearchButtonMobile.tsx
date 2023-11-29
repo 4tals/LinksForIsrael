5b4b9ad2c32191eeaae2c16c26454540f3a23efe
Alwyn <idan.4tal@gmail.com>
2023-11-30 00:21:12 +0200

    Convert two more compoents to Chakra and remove more CSS

--- app/components/Search/SearchButtonMobile.tsx
+++ app/components/Search/SearchButtonMobile.tsx
@@ -1,33 +1,22 @@
-"use client";
-
-import { useContext } from "react";
+import React, { useContext } from "react";
 
 import { SearchContext } from "@/app/components/RootApp";
+import { SearchIcon } from "@chakra-ui/icons";
+import { IconButton } from "@chakra-ui/react";
 
 export function SearchButtonMobile() {
 	const { toggleMobileSearch } = useContext(SearchContext);
+
 	return (
-		<>
-			<button
-				className="mobile-search-button"
-				type="button"
-				onClick={toggleMobileSearch}
-			>
-				<svg
-					width="23"
-					height="23"
-					viewBox="0 0 23 23"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						fillRule="evenodd"
-						clipRule="evenodd"
-						d="M0 10.192C0 4.5635 4.561 0 10.1875 0C15.814 0 20.3745 4.5635 20.3745 10.192C20.3745 12.1775 19.807 14.031 18.825 15.598C18.9735 15.722 19.1215 15.8445 19.2695 15.967L19.275 15.972C20.092 16.6495 20.903 17.322 21.704 18.065C22.6655 18.9565 22.7985 20.3435 21.864 21.31C21.6828 21.4976 21.4984 21.6821 21.311 21.8635C20.345 22.7985 18.9585 22.666 18.0675 21.7035C17.3255 20.902 16.653 20.0905 15.976 19.273L15.973 19.269L15.972 19.268L15.605 18.8255C13.9829 19.8469 12.1044 20.3875 10.1875 20.3845C4.561 20.3845 0 15.821 0 10.192ZM10.25 16.75C13.84 16.75 16.75 13.84 16.75 10.25C16.75 6.66 13.84 3.75 10.25 3.75C6.66 3.75 3.75 6.66 3.75 10.25C3.75 13.84 6.66 16.75 10.25 16.75Z"
-						fill="white"
-					/>
-				</svg>
-			</button>
-		</>
+		<IconButton
+			aria-label="Search"
+			icon={<SearchIcon />}
+			onClick={toggleMobileSearch}
+			size="lg"
+			colorScheme="blue"
+			borderRadius="full"
+			variant="solid"
+			color="white"
+		/>
 	);
 }
--- app/components/Search/SearchInput.tsx
+++ app/components/Search/SearchInput.tsx
@@ -1,13 +1,14 @@
-"use client";
-
-import { useContext, useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 
 import { MenuContext, SearchContext } from "@/app/components/RootApp";
+import { Search2Icon } from "@chakra-ui/icons";
+import { Box, Input, Icon, useMediaQuery } from "@chakra-ui/react";
 
 export function SearchInput() {
 	const { search, onSearch, isMobileSearchOpen } = useContext(SearchContext);
 	const { isMenuOpen, toggleMenu } = useContext(MenuContext);
 	const inputRef = useRef<HTMLInputElement>(null);
+	const [isMobile] = useMediaQuery("(max-width: 768px)");
 
 	useEffect(() => {
 		if (isMobileSearchOpen) {
@@ -16,43 +17,44 @@ export function SearchInput() {
 	}, [isMobileSearchOpen]);
 
 	return (
-		<div className="search-wrapper">
-			<svg
-				className="mobile-search-icon"
-				width="24"
-				height="24"
-				viewBox="0 0 24 24"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					fillRule="evenodd"
-					clipRule="evenodd"
-					d="M6.19348 6.0194C8.88602 3.32687 13.2515 3.32687 15.944 6.0194C18.4395 8.51489 18.6221 12.4474 16.4919 15.1532L19.9998 18.6611L18.9391 19.7218L15.4425 16.2251C12.7341 18.4528 8.72461 18.3011 6.19348 15.77C3.50094 13.0774 3.50094 8.71194 6.19348 6.0194ZM14.8834 7.08007C12.7766 4.97331 9.36089 4.97331 7.25414 7.08007C5.14738 9.18682 5.14738 12.6025 7.25414 14.7093C9.36089 16.816 12.7766 16.816 14.8834 14.7093C16.9901 12.6025 16.9901 9.18682 14.8834 7.08007Z"
-					fill="#71717A"
-				/>
-			</svg>
-			<input
+		<Box
+			display="flex"
+			alignItems="center"
+			backgroundColor="white"
+			width={isMobile ? "100%" : "400px"}
+			height={isMobile ? "35px" : "44px"}
+			borderRadius="20px"
+			padding="0 10px"
+		>
+			<Icon
+				as={Search2Icon}
+				color="gray.500"
+				w={6}
+				h={6}
+				display={{ base: "none", md: "flex" }}
+			/>
+			<Input
 				ref={inputRef}
-				className="search-input"
 				id="searchInputWeb"
 				name="query"
 				placeholder="מה תרצו לחפש?"
-				list="search-suggestions"
 				autoComplete="off"
 				value={search}
 				onChange={(e) => {
 					onSearch(e.target.value);
-					isMobileSearchOpen && isMenuOpen && toggleMenu();
+					if (isMobileSearchOpen && isMenuOpen) {
+						toggleMenu();
+					}
 				}}
+				border="none"
+				outline="none"
+				marginRight="7px"
+				width="100%"
+				list="search-suggestions"
+				borderRadius="20px"
+				bgColor="transparent" // Set background to transparent
+				_focus={{ boxShadow: "none" }} // Remove focus outline
 			/>
-			<datalist id="search-suggestions">
-				<option value="התנדבות" />
-				<option value="תרומה" />
-				<option value="סושיאל" />
-				<option value="ילדים" />
-				<option value="אירוח משפחות" />
-			</datalist>
-		</div>
+		</Box>
 	);
 }
